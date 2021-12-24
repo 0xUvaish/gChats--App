141eda4425d88e8b5ca1205c5f48cd37c7b45cc8
Mohd Uvaish Siddiqui <70436834+0xUvaish@users.noreply.github.com>
2021-12-24 17:58:21 +0530

    All files uploaded

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed'
+
+import './App.css';
+import LoginForm from './components/LoginForm';
+
+const App = () => {
+
+    if (!localStorage.getItem('username')) 
+    return (<LoginForm />);
+
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID= {process.env.REACT_APP_PROJECT_ID}
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
+        />
+    );
+}
+
+export default App;
